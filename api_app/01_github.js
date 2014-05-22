a19f5f3a621f390b7486802b3c5516fabb9e715c
Shane Perera <sperera@avg.com.au>
2014-05-22 15:42:17 +1000

    Wrap the API processing in a function

--- api_app/01_github.js
+++ api_app/01_github.js
@@ -2,38 +2,44 @@
 
 var https = require('https');
 
-var username = 'OllieParsley';
 
-var options = {
-	host: 'api.github.com',
-	path: '/users/' + username + '/repos',
-	method: 'GET',
-	headers: {
-		'User-Agent': 'MyNodeApp'
-	}
-};
+function getRepositories(username, callback) {
+	var options = {
+		host: 'api.github.com',
+		path: '/users/' + username + '/repos',
+		method: 'GET',
+		headers: {
+			'User-Agent': 'MyNodeApp'
+		}
+	};
 
 
-var request = https.request(options, function(response) {
-	// concat all the pieces of data received
-	var body = '';
-	response.on('data', function(chunk) {
-		body += chunk.toString('utf8');
-	});
-
-	response.on('end', function() {
-		var json = JSON.parse(body);
-		var repositories = [];
-		json.forEach(function(repo) {
-			repositories.push({
-				name: repo.name,
-				description: repo.description
-			});
+	var request = https.request(options, function(response) {
+		// concat all the pieces of data received
+		var body = '';
+		response.on('data', function(chunk) {
+			body += chunk.toString('utf8');
 		});
-		console.log('Total is ' + json.length);
-		console.log(repositories);
-	});
-});
 
-// this is where we actually send the request to get data
-request.end();
+		response.on('end', function() {
+			var json = JSON.parse(body);
+			var repositories = [];
+			json.forEach(function(repo) {
+				repositories.push({
+					name: repo.name,
+					description: repo.description
+				});
+			});
+			
+			callback(repositories);
+		});
+	});
+
+	// this is where we actually send the request to get data
+	request.end();
+}
+
+
+getRepositories('shanecp', function(repositories) {
+	console.log('Shane has ' + repositories.length + ' repos.');
+});
